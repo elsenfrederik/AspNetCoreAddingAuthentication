a4fcb4e89efdbe1214ddf049eec52ec5adcd3d95
Frederik Elsen <elsen_frederik@hotmail.com>
2019-03-13 10:51:35 +0100

    Completed this module

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -7,7 +7,6 @@ namespace WishList.Controllers
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
-
         public ItemController(ApplicationDbContext context)
         {
             _context = context;
