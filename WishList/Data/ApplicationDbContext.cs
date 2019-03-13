77371fab8fe378e69356ccb764403c38189a976d
Frederik Elsen <elsen_frederik@hotmail.com>
2019-03-13 09:48:08 +0100

    Completed this module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+           
         }
 
         public DbSet<Models.Item> Items { get; set; }
